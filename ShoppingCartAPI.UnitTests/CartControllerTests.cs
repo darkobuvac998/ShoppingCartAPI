a44f41c94747f20e25ffb9dc4ad003fb724ebc20
darkobuvac <darko.buvac@codaxy.com>
2023-01-31 22:14:24 +0100

    added unit tests

---
+++ ShoppingCartAPI.UnitTests/CartControllerTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ShoppingCart.API.Controllers;
+using ShoppingCart.Contracts.IServices;
+using ShoppingCart.Entities.DTOs.Cart;
+using ShoppingCart.Entities.Models;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ShoppingCart.Tests
+{
+    public class CartControllerTests
+    {
+        private readonly CartController _sut;
+        private readonly Mock<IServiceManager> _serviceManagerMock = new Mock<IServiceManager>();
+        public CartControllerTests()
+        {
+            _sut = new CartController(_serviceManagerMock.Object);
+        }
+
+        [Fact]
+        public async Task GetCartAsync_ShouldReturnOk_WhenItemExists()
+        {
+            // Arrange
+            var cartId = It.IsAny<int>();
+            var cart = new CartDto
+            {
+                CartId = cartId,
+                CreatedBy = "Test User",
+                Status = CartStatus.Submited.ToString(),
+            };
+
+            _serviceManagerMock.Setup(x => x.CartService.GetCartAsync(cartId, false)).ReturnsAsync(cart);
+
+            // Act
+            var result = await _sut.GetCartAsync(cartId);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task GetCartAsync_ShouldReturnNotFound_WhenCartDoesNotExists()
+        {
+            // Arrange
+            var cartId = It.IsAny<int>();
+
+            _serviceManagerMock.Setup(x => x.CartService.GetCartAsync(cartId, false)).ReturnsAsync(() => null);
+
+            // Act
+            var result = await _sut.GetCartAsync(cartId); 
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task CancelCartAsync_ShouldReturnNoContentResult_WhenCartExistsAndWhenStatusChanged()
+        {
+            // Arrange
+            var cartId = It.IsAny<int>();
+
+            _serviceManagerMock.Setup(x => x.CartService.CancelCartAsync(cartId)).ReturnsAsync(true);
+
+            // Act
+            var result = await _sut.CancelCartAsync(cartId);
+
+            // Assert
+            _serviceManagerMock.Verify(v => v.CartService.CancelCartAsync(cartId), Times.Once);
+            Assert.IsType<NoContentResult>(result);
+        }
+
+        [Fact]
+        public async Task CancelCartAsync_ShouldReturnBadRequest_WhenCartDoesNotExists()
+        {
+            // Arrange
+            var cartId = It.IsAny<int>();
+
+            _serviceManagerMock.Setup(x => x.CartService.CancelCartAsync(cartId)).ReturnsAsync(false);
+
+            // Act
+            var result = await _sut.CancelCartAsync(cartId);
+
+            // Assert
+            Assert.IsType<BadRequestResult>(result);
+        }
+    }
+}
---
+++ ShoppingCartAPI.UnitTests/CartServiceTests.cs
@@ -0,0 +1,152 @@
+using AutoMapper;
+using Moq;
+using ShoppingCart.Contracts.IRepositories;
+using ShoppingCart.Entities.DTOs.Cart;
+using ShoppingCart.Entities.Models;
+using ShoppingCart.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ShoppingCart.Tests
+{
+    public class CartServiceTests
+    {
+        private readonly CartService _sut;
+        private readonly Mock<IRepositoryManager> _repoManagerMock = new Mock<IRepositoryManager>();
+        private readonly Mock<IMapper> _mapperMock = new Mock<IMapper>();
+
+        public CartServiceTests()
+        {
+            _sut = new CartService(_repoManagerMock.Object, _mapperMock.Object);
+        }
+
+        [Fact]
+        public async Task GetCartAsync_ShouldReturnCart_WhenCartExists()
+        {
+            // Arrange
+            var cartId = 1;
+            var cart = new Cart
+            {
+                CartId = cartId,
+                Status = CartStatus.Draft,
+                TimeCreated = DateTime.Now,
+                TimeUpdated = DateTime.Now,
+                CreatedBy = "UserTest",
+                CartItems = new List<CartItem>
+                {
+                    new CartItem
+                    {
+                        CartId = cartId,
+                        CartItemId = 1,
+                        Amount = 2,
+                        CreatedBy = "UserTest",
+                        Name = "Item 1",
+                        Description = "Desc 1",
+                        TimeCreated = DateTime.Now,
+                        TimeUpdated = DateTime.Now,
+                    }
+                }
+            };
+            var cartDto = new CartDto
+            {
+                CartId = cartId,
+                Status = "Draft",
+                TimeCreated = DateTime.Now,
+                TimeUpdated = DateTime.Now,
+                CreatedBy = "UserTest",
+                CartItems = new List<CartItemShortDto>
+                {
+                    new CartItemShortDto
+                    {
+                        Name = "Item 1",
+                        CartItemId = 1
+                    }
+                }
+            };
+
+            _repoManagerMock.Setup(x => x.Carts.GetCartAsync(cartId, false)).ReturnsAsync(cart);
+            _mapperMock.Setup(x => x.Map<CartDto>(It.IsAny<Cart>())).Returns(cartDto);
+
+            // Act
+            var result = await _sut.GetCartAsync(cartId, false);
+
+            // Assert
+            Assert.Equal(cartDto, result);
+        }
+
+        [Fact]
+        public async Task GetCartAsync_ShouldReturnNothing_WhenCartNotExists()
+        {
+            // Arrange
+            var cartId = 1;
+            _repoManagerMock.Setup(x => x.Carts.GetCartAsync(cartId, false)).ReturnsAsync(() => null);
+
+            // Act
+            var result = await _sut.GetCartAsync(cartId, false);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task CancelCartAsync_ShouldChangeStatusToCancelled_WhenCartExists()
+        {
+            // Arrange
+            var cartId = 1;
+            var cart = new Cart
+            {
+                CartId = cartId,
+                Status = CartStatus.Draft,
+                TimeCreated = DateTime.Now,
+                TimeUpdated = DateTime.Now,
+                CreatedBy = "UserTest",
+                CartItems = new List<CartItem>
+                {
+                    new CartItem
+                    {
+                        CartId = cartId,
+                        CartItemId = 1,
+                        Amount = 2,
+                        CreatedBy = "UserTest",
+                        Name = "Item 1",
+                        Description = "Desc 1",
+                        TimeCreated = DateTime.Now,
+                        TimeUpdated = DateTime.Now,
+                    }
+                }
+            };
+
+            _repoManagerMock.Setup(x => x.Carts.GetCartAsync(cartId, true)).ReturnsAsync(cart);
+
+            // Act
+            var result = await _sut.CancelCartAsync(cartId);
+
+            // Assert
+            _repoManagerMock.Verify(v => v.Carts.Update(cart), Times.Once);
+            _repoManagerMock.Verify(v => v.SaveAsync(), Times.Once);
+            Assert.Equal(CartStatus.Cancelled, cart.Status);
+            Assert.Equal(DateTime.Now.ToString("HH:mm:ss"), cart.TimeUpdated.ToString("HH:mm:ss"));
+            Assert.True(result);
+
+        }
+
+        [Fact]
+        public async Task CancelCartAsync_ShouldNotCallSaveChanges_WhenCartDoesNotExists()
+        {
+            // Arrange
+            var cartId = 1;
+            var itemId = 1;
+
+            _repoManagerMock.Setup(x => x.Carts.GetCartAsync(cartId, true)).ReturnsAsync(() => null);
+
+            // Act
+            var result = await _sut.CancelCartAsync(cartId);
+
+            // Assert
+            _repoManagerMock.Verify(v => v.SaveAsync(), Times.Never);
+            Assert.False(result);
+        }
+    }
+}
