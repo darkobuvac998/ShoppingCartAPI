822773f1817302d550e67fe3c62ef8f19f841ea2
darkobuvac <darko.buvac@codaxy.com>
2023-02-10 13:40:51 +0100

    test

---
+++ Jenkins/create-image-folder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+folder_path=$1
+
+if [ ! -d "$folder_path" ]; then
+  mkdir "$folder_path"
+  echo "Folder '$folder_path' has been created."
+else
+  echo "Folder '$folder_path' already exists."
+fi
