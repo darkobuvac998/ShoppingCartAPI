5ab471eb56101b9a7c1890d709cf5a6e310549e6
darkobuvac <darko.buvac@codaxy.com>
2023-02-10 11:14:12 +0100

    fix shell script

--- Jenkins/generate-html-report.sh
+++ Jenkins/generate-html-report.sh
@@ -8,7 +8,9 @@ echo "<body>" >>index.html
 echo "<h1>Build Report</h1>" >>index.html
 echo "<p>Build Number: $BUILD_NUMBER</p>" >>index.html
 echo "<p>Build Status: $BUILD_STATUS</p>" >>index.html
-echo "<h1>Build Report for Job: $JOB_NAME</h1>" >>report.html
-echo "<h2>Build Number: $BUILD_NUMBER</h2>" >>report.html
+echo "<h1>Build Report for Job: $JOB_NAME</h1>" >>index.html
+echo "<h2>Build Number: $BUILD_NUMBER</h2>" >>index.html
 echo "</body>" >>index.html
 echo "</html>" >>index.html
+
+mv index.html ./reports
