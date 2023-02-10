113932a3de34e6a4619ea0c316b21847fe0ce778
darkobuvac <darko.buvac@codaxy.com>
2023-02-10 11:07:09 +0100

    add generate report shell script

---
+++ Jenkins/generate-html-report.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "<html>" >index.html
+echo "<head>" >>index.html
+echo "<title>Build Report</title>" >>index.html
+echo "</head>" >>index.html
+echo "<body>" >>index.html
+echo "<h1>Build Report</h1>" >>index.html
+echo "<p>Build Number: $BUILD_NUMBER</p>" >>index.html
+echo "<p>Build Status: $BUILD_STATUS</p>" >>index.html
+echo "<h1>Build Report for Job: $JOB_NAME</h1>" >>report.html
+echo "<h2>Build Number: $BUILD_NUMBER</h2>" >>report.html
+echo "</body>" >>index.html
+echo "</html>" >>index.html
